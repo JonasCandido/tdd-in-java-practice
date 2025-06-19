089e72a1088391e9e31422109d61d7bb43a2ec35
JonasCandido <jonasdcandido210@gmail.com>
2025-06-19 15:19:29 -0300

    Fix test

--- tddkb/tdd-money/src/main/java/com/jonas/money/tddmoney/Dollar.java
+++ tddkb/tdd-money/src/main/java/com/jonas/money/tddmoney/Dollar.java
@@ -1,16 +1,11 @@
 package com.jonas.money.tddmoney;
 
 class Dollar extends Money {
-    private String currency;
     public Dollar(int amount, String currency) {
         super(amount, currency);
     }
 
     Money times(int multiplier) {
         return Money.dollar(amount * multiplier);
-    }
-
-    String currency() {
-        return currency;
     }
 }
--- tddkb/tdd-money/src/main/java/com/jonas/money/tddmoney/Franc.java
+++ tddkb/tdd-money/src/main/java/com/jonas/money/tddmoney/Franc.java
@@ -1,16 +1,11 @@
 package com.jonas.money.tddmoney;
 
 class Franc extends Money {
-    private String currency;
     public Franc(int amount, String currency) {
         super(amount, currency);
     }
 
     Money times(int multiplier) {
         return Money.franc(amount * multiplier);
-    }
-
-    String currency() {
-        return currency;
     }
 }
